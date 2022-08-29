6449cae9ebbcd71ea13d3caa2082c49b4d030333
mehulbhange <bhangemehul@gmail.com>
2022-08-29 15:20:57 +0530

    ADDED:[Mehul] added welcome message

---
+++ src/com/bridgelabz/StackMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+/**
+ * Purpose - To perform stack and queue operations
+ * @author - Mehul Bhange
+ * @since - 29-08-2022
+ */
+public class StackMain {
+    public static void main(String[] args) {
+        System.out.println("Stacks and Queues program");
+
+
+    }
+}
